c7f16ec73e05783dc5c6b1ada96a4b8668879392
github username <github email address>
2021-03-28 10:29:07 -0500

    added graphQL from AWS Amplify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import { useEffect } from "react";
 import { DataStore } from "@aws-amplify/datastore";
-import { User } from "./models";
+import { User, Stacks } from "./models";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
   let addUser = async () => {
@@ -14,9 +18,30 @@ function App() {
 
     console.log(newUser);
   };
+
+  //original userID: 148ee1c0-b57b-4c37-8ae8-394982f6ef56
+  let addStack = async () => {
+    const stack = {
+      title: window.prompt("title"),
+      userID: "7f0e7347-7026-4dba-8d05-69b3be2521cb",
+      Cards: [],
+    };
+    const newStack = await DataStore.save(new Stacks(stack));
+
+    console.log(newStack);
+  };
+
+  useEffect(() => {
+    const func = async () => {
+      const models = await DataStore.query(Stacks);
+      console.log(models);
+    };
+    func();
+  }, []);
   return (
     <div className="App">
       <button onClick={() => addUser()}> Add User</button>
+      <button onClick={() => addStack()}> Add Stack</button>
     </div>
   );
 }
